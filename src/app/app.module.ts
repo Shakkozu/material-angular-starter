04ed4a1e3db32cff2a6db16bc584e37071908efc
adam.kolodziejczyk <shakkozuu@gmail.com>
2024-06-09 22:23:54 +0200

    Update @angular/cli and @angular/cdk to v18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,36 +13,29 @@ import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { WeatherForecastComponent } from './weather-forecast/weather-forecast.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { MatDashboardComponent } from './mat-dashboard/mat-dashboard.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    WeatherForecastComponent,
-    MatDashboardComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatGridListModule,
-    MatCardModule,
-    MatMenuModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        WeatherForecastComponent,
+        MatDashboardComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatIconModule,
+        MatListModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatSortModule,
+        MatGridListModule,
+        MatCardModule,
+        MatMenuModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
